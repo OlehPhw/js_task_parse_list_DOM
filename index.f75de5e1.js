64560899dd9a4f08694fc73d8401214201123a5f
Oleh K <olegk0409@gmail.com>
2024-06-11 12:04:35 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+const names = document.querySelectorAll("li");
+function sortBySalary(nodeList) {
+    const array = [
+        ...nodeList
+    ];
+    array.sort((a, b)=>{
+        const normalizedA = +a.dataset.salary.replaceAll("$", "").replaceAll(",", "");
+        const normalizedB = +b.dataset.salary.replaceAll("$", "").replaceAll(",", "");
+        return normalizedB - normalizedA;
+    });
+    const parentUl = nodeList[0].parentNode;
+    array.forEach((node)=>parentUl.appendChild(node));
+}
+function createArrayOfObjects(nodeList) {
+    return [
+        ...nodeList
+    ].map((el)=>({
+            name: el.textContent.replaceAll("\n", "").replaceAll(" ", ""),
+            position: el.dataset.position,
+            salary: el.dataset.salary,
+            age: el.dataset.age
+        }));
+}
+sortBySalary(names);
+createArrayOfObjects(names);
+
+//# sourceMappingURL=index.f75de5e1.js.map
